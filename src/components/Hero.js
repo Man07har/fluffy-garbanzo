8c6686037048608eac657f212a806096a9bae46d
Man07har <thakurmanoharkumar7@gmail.com>
2022-11-11 23:06:38 -0800

    Few properties changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,10 +9,10 @@ const hero = () => {
 <h1 className='text-[50px] flex justify-center mx-[100px] my-[80px]'>
    <span className='font-black text-[72px]'>BURGER</span> WEEK
 </h1>
-<div classname="text-yellow-400 flex mx-[10px]">
-  <h1 className='font-black text-[50px]'>BURGER YOU <span className='text-red-700'>"DEMAND"</span></h1>
-  <h1 className='font-black text-[50px]'>TASTE WE<span className='text-red-700'>"DELIVER"</span> </h1>
-  <a><span className='text-red-700 text-[72px] hover:text-[75px] '>ORDER NOW</span> </a>
+<div className="text-yellow-400 mx-[10px]">
+  <h1 className='font-black text-[50px]'>{`BURGER YOU `}<span className='text-red-700'>{`"DEMAND"`}</span></h1>
+  <h1 className='font-black text-[50px]'>{`TASTE WE`}<span className='text-red-700'>{`"DELIVER"`}</span> </h1>
+  <a><span className='text-red-700 text-[72px] hover:text-[75px] '>{`ORDER NOW`}</span> </a>
 </div>
  </div>
  <div className='Image'>
